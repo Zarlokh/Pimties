6acc49ebbcf2654556d90e7f88e0d09df43bb5a1
Zarlokh <raphael.b68700@gmail.com>
2022-11-05 12:50:53 +0100

    main : Ne peut avoir qu'une configuration par type de produit

--- src/EventSubscriber/OnBeforeProductConfigurationEvent.php
+++ src/EventSubscriber/OnBeforeProductConfigurationEvent.php
@@ -30,7 +30,7 @@ class OnBeforeProductConfigurationEvent implements EventSubscriberInterface
     public function onBeforeNewProductConfiguration(BeforeCrudActionEvent $event): void
     {
         $adminContext = $event->getAdminContext();
-        if (!$adminContext || $adminContext->getEntity()->getName() !== AbstractProductConfiguration::class || Action::NEW !== $adminContext->getCrud()?->getCurrentAction()) {
+        if (!$adminContext || $adminContext->getEntity()->getFqcn() !== AbstractProductConfiguration::class || Action::NEW !== $adminContext->getCrud()?->getCurrentAction()) {
             return;
         }
         $subclass = $adminContext->getRequest()->query->get('sub_class');
